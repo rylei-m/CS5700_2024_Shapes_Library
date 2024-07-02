410dba7826d56fc82cd2e321709999ece5042de2
Rylei Mindrum <rymindrum@gmail.com>
2024-07-01 18:57:33 -0600

    created movement class for better encapsulation

---
+++ src/main/kotlin/shapes/Movement.kt
@@ -0,0 +1,7 @@
+package org.example.shapes
+
+class Movement {
+    fun move(deltaX: Double, deltaY: Double, deltaZ: Double) {
+        Unit
+    }
+}
--- src/main/kotlin/shapes/Point.kt
+++ src/main/kotlin/shapes/Point.kt
@@ -2,6 +2,8 @@ package org.example.shapes
 
 data class Point(var x: Double, var y: Double) {
     fun clone(): Point = Point(x,y)
+
+    // use movement here instead
     fun move(deltaX: Double, deltaY: Double) {
         x += deltaX
         y += deltaY
