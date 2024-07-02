965029a6013392dd10140f3a9fc8dffb134b8da7
Rylei Mindrum <rymindrum@gmail.com>
2024-07-02 09:40:13 -0600

    rectangle basic

--- src/main/kotlin/shapes/polygons/Rectangle.kt
+++ src/main/kotlin/shapes/polygons/Rectangle.kt
@@ -1,4 +1,18 @@
 package org.example.shapes.Polygons
 
-class Rectangle {
+class Rectangle(
+    val topLeft: Point,
+    val topRight: Point) {
+
+    init {
+        require(topLeft.x != bottomRight.x && topLeft.y != bottomRight.y) {"Rectangle cannot have 0 width or height"}
+    }
+
+    fun getTopLeft(): Point = topLeft.clone()
+    fun getBottomRight(): Point = bottomRight.clone()
+    fun getArea(): Double = (bottomRight.x - topLeft.x) * (bottomRight.y - topLeft.y)
+    fun move(deltaX: Double, deltaY: Double) {
+        topLeft.move(deltaX, deltaY)
+        BottomRight.move(deltaX, deltaY)
+    }
 }
