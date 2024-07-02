e84ae59964476d35a8d3427ba98489452ff0ac63
Rylei Mindrum <rymindrum@gmail.com>
2024-07-02 17:46:06 -0600

    Fix Rectangle for modularity and use RULE

--- src/main/kotlin/shapes/polygons/Rectangle.kt
+++ src/main/kotlin/shapes/polygons/Rectangle.kt
@@ -4,38 +4,10 @@ import shapes.Point
 import shapes.Shape
 import shapes.polygons.Rule
 
-open class Rectangle(
-    private val _topLeft: Point,
-    private val _bottomRight: Point
-) : Shape(listOf(_topLeft, _bottomRight)), Rule {
-
-    init {
-        require(_topLeft.x != _bottomRight.x && _topLeft.y != _bottomRight.y) {
-            "Rectangle cannot have 0 width or height"
-        }
-    }
-
-    val topLeft: Point
-        get() = _topLeft.clone();
-
-    val bottomRight: Point
-        get() = _bottomRight.clone();
-
-    override fun area(): Double =abs(_bottomRight.x -_topLeft.x) * abs(_bottomRight.y -_topLeft.y)
-
-    override fun move(deltaX: Double, deltaY: Double) {
-        _topLeft.move(deltaX, deltaY)
-        _bottomRight.move(deltaX, deltaY)
-    }
-
-    fun getTopLeft(): Point = _topLeft.clone()
-    fun getBottomRight(): Point = _bottomRight.clone()
+open class Rectangle: Rule {
     override fun isValidShape(points: List<Point>) {
-        TODO("Not yet implemented")
+        require(points.size == 2) {"Rectangle needs to have exactly 2 points"}
+        require(points[0] != points[1]) {"Rectangle points cant be the same"}
     }
-
-    override fun area(points: List<Point>): Double {
-        TODO("Not yet implemented")
-    }
-
+    override fun area(points: List<Point>): Double = abs(points[1].x - points[0].x) * abs(points[1].y - points[0].y)
 }
--- src/main/kotlin/shapes/polygons/Square.kt
+++ src/main/kotlin/shapes/polygons/Square.kt
@@ -1,28 +1,12 @@
-package shapes.Polygons
+package shapes.polygons
 
 import shapes.Point
 import kotlin.math.abs
 
-class Square(
-    private val _topLeft: Point,
-    private val _size: Double
-) : Rectangle(_topLeft, Point(_topLeft.x + _size, _topLeft.y + _size)) {
-
-    init {
-        require(_size > 0.0) { "The square size must be greater than zero." }
-    }
-
+class Square: Rectangle(), Rule {
     override fun isValidShape(points: List<Point>) {
-        super.isValidShape(points)
-        val side1 = abs(points[0].x - points[1].x)
-        val side2 = abs(points[1].y - points[2].y)
-        require(side1 == side2) {"all sides of a square have to be equal"}
-    }
-
-    fun area(): Double = _size * _size
-
-    override fun move(deltaX: Double, deltaY: Double) {
-        _topLeft.move(deltaX, deltaY)
-        _bottomRight.move(deltaX, deltaY)
+        super.isValidShape(points: List<Point>) {
+            require(abs(points[0].x - points[0].x) == abs(points[0].y - points[1].y)) {"Width and height have to be equal"}
+        }
     }
 }
