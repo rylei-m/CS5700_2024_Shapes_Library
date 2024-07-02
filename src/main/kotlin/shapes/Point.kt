2fe7a33dff6a51f5c7a1dbb0a314a09b8a16e9c0
Rylei Mindrum <rymindrum@gmail.com>
2024-07-02 08:37:12 -0600

    fixed co-pilot oversight. added overrides

--- src/main/kotlin/shapes/Point.kt
+++ src/main/kotlin/shapes/Point.kt
@@ -6,18 +6,26 @@ class Point(
         private var _x: Double = x
         private var _y: Double = y
     val x: Double
-        get() = this._x
+        get() = _x
     val y: Double
         get() = _y
-    fun clone(): Point = Point(x, y)
+    fun clone(): Point = Point(_x, _y)
     override fun movement(deltaX: Double, deltaY: Double) {
-        this._x += deltaX
-        this._y += deltaY
+        _x += deltaX
+        _y += deltaY
 
     }
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Point) return false
         return _x == other._x && _y == other._y
+    }
+
+    // 31 cause hashcode needs a prime number and 31 is most standard
+    override fun hashCode(): Int {
+        return 31 * _x.hashCode() + _y.hashCode()
+    }
+    override fun toString(): String {
+        return "Point(x=$_x, y=$_y)"
     }
 }
