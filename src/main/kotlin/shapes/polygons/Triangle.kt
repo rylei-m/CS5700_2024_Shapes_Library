b4b37787949e38af91e50a7b3467658306fcc0c3
Rylei Mindrum <rymindrum@gmail.com>
2024-07-02 10:43:51 -0600

    Refactor Triangle

--- src/main/kotlin/shapes/polygons/Triangle.kt
+++ src/main/kotlin/shapes/polygons/Triangle.kt
@@ -1,23 +1,15 @@
 package shapes.polygons
-import org.example.shapes.Point
-import java.awt.Polygon
+import shapes.Point
 import kotlin.math.abs
 
-class Triangle(val point1: Point, val point2: Point, val point3: Point) : Polygon {
-    init {
-        require(getArea() > 0) {
-            "Triangle must have a area"
-        }
+class Triangle: Rule {
+    override fun isValidShape(points: List<Point>) {
+        require(points.size != 3) {"Triangle must have exactly 3 points"}
+        require(area(points) != 0.0) { "Triangle must have a area" }
     }
-    fun isValidShape(points: List<Point>) {
-        if (points.size != 3) {
-            throw IllegalArgumentException("Triangle must have exactly 3 points")
-        }
-        if (area(points) == 0.0) {
-            throw IllegalArgumentException("The area of a triangle cannot be 0")
-        }
-    }
-    fun area(points: List<Point>): Double =
-        abs(points[0].x * (points[1].y - points[2].y) + points[1].x * (points[2].y - points[0].y) + points[2].x * (points[0].y - points[1].y)) /2
-
+    override fun area(points: List<Point>): Double =
+        abs(points[0].x * (points[1].y - points[2].y) +
+                points[1].x * (points[2].y - points[0].y) +
+                points[2].x * (points[0].y - points[1].y)
+        ) /2
 }
