02c84d5eff4c8aa9f381522fb7135829ab1e8b0f
Rylei Mindrum <rymindrum@gmail.com>
2024-07-02 09:56:31 -0600

    Square Start

--- src/main/kotlin/shapes/polygons/Rectangle.kt
+++ src/main/kotlin/shapes/polygons/Rectangle.kt
@@ -1,12 +1,21 @@
 package org.example.shapes.Polygons
+import kotlin.math.abs
+import shapes.Point
 
-class Rectangle(
+open class Rectangle(
     val topLeft: Point,
-    val topRight: Point) {
+    val bottomRight: Point) {
 
     init {
         require(topLeft.x != bottomRight.x && topLeft.y != bottomRight.y) {"Rectangle cannot have 0 width or height"}
     }
+    override fun isValidShape(points: List<Point>) {
+        if (topLeft[0] == topRight[0]) {"Rectangle points cannot be equal"}
+        if (points.size != 2) throw RuntimeException("Incorrect number of points")
+    }
+
+    override fun area(points: List<Point>): Double = abs(bottomRight.x - topLeft.x) * abs(bottomRight.y - topLeft.y)
+
 
     fun getTopLeft(): Point = topLeft.clone()
     fun getBottomRight(): Point = bottomRight.clone()
