b8f52dcaeb14d77c17460aa173fdd826c3a2ccc5
Rylei Mindrum <rymindrum@gmail.com>
2024-07-02 09:10:21 -0600

    fixed math equations, formatting, added overrides

--- src/main/kotlin/shapes/Line.kt
+++ src/main/kotlin/shapes/Line.kt
@@ -1,14 +1,30 @@
 package shapes
 import shapes.Point
 import shapes.Movement
+import kotlin.math.sqrt
+import kotlin.math.pow
 
-class Line(val start: Point, val end: Point) {
+class Line(
+    private val _start: Point,
+    private val _end: Point
+): Movement {
     init {
-        require(start != end) { "Line length can't be equal to 0." }
+        require(_start != _end) { "Line length can't be equal to 0." }
     }
+    val start: Point
+        get() = _start.clone()
+
+    val end: Point
+        get() = _end.clone()
+
     fun getStart(): Point = start.clone()
     fun getEnd(): Point = end.clone()
     fun getSlope(): Double = (end.y - start.y) / (end.x - start.x)
-    fun getLength(): Double = Math.hypot(end.x - start.x, end.y - start.y)
-    Movement()
+    fun getLength(): Double = sqrt((_end.x - _start.x).pow(2) + (_end.y - _start.y).pow(2))
+
+    override fun move(deltaX: Double, deltaY: Double) {
+        _start.move(deltaX, deltaY)
+        _end.move(deltaX, deltaY)
+    }
+    override fun toString(): String = "Line($start, $end)"
 }
