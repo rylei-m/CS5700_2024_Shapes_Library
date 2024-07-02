d208613b2ee0003660c6ac5e82200de232e0cc55
Rylei Mindrum <rymindrum@gmail.com>
2024-07-02 08:40:10 -0600

    Fix Formatting Errors

--- src/main/kotlin/shapes/Point.kt
+++ src/main/kotlin/shapes/Point.kt
@@ -1,20 +1,25 @@
-package org.example.shapes
+package shapes
 
 class Point(
     x: Double,
-    y: Double): Movement {
-        private var _x: Double = x
-        private var _y: Double = y
+    y: Double
+): Movement {
+    private var _x: Double = x
+    private var _y: Double = y
+
     val x: Double
         get() = _x
+
     val y: Double
         get() = _y
+
     fun clone(): Point = Point(_x, _y)
-    override fun movement(deltaX: Double, deltaY: Double) {
+
+    override fun move(deltaX: Double, deltaY: Double) {
         _x += deltaX
         _y += deltaY
-
     }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Point) return false
