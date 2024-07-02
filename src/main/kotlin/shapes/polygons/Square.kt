e1a71d148e13828173941d5780334d0993600e55
Rylei Mindrum <rymindrum@gmail.com>
2024-07-02 17:47:05 -0600

    removal of broken code

--- src/main/kotlin/shapes/polygons/Square.kt
+++ src/main/kotlin/shapes/polygons/Square.kt
@@ -1,12 +1,12 @@
 package shapes.polygons
 
 import shapes.Point
+import shapes.Polygons.Rectangle
 import kotlin.math.abs
 
 class Square: Rectangle(), Rule {
     override fun isValidShape(points: List<Point>) {
-        super.isValidShape(points: List<Point>) {
+        super.isValidShape(points)
             require(abs(points[0].x - points[0].x) == abs(points[0].y - points[1].y)) {"Width and height have to be equal"}
-        }
     }
 }
