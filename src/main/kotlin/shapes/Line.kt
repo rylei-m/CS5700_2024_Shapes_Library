192fb44c2bc0ddf5c12d8609b68848bf4cfbcc84
Rylei Mindrum <rymindrum@gmail.com>
2024-07-02 08:46:26 -0600

    Math, make sure line isn't equal to 0, movement to use Movement

--- src/main/kotlin/shapes/Line.kt
+++ src/main/kotlin/shapes/Line.kt
@@ -1,14 +1,14 @@
-package org.example.shapes
+package shapes
+import shapes.Point
+import shapes.Movement
 
 class Line(val start: Point, val end: Point) {
     init {
-
+        require(start != end) { "Line length can't be equal to 0." }
     }
     fun getStart(): Point = start.clone()
     fun getEnd(): Point = end.clone()
-    fun getSlope(): Double = 0.00 // slope math
-    fun getLength(): Double = 0.00 //slope math
-    fun move(){
-        //movement
-    }
+    fun getSlope(): Double = (end.y - start.y) / (end.x - start.x)
+    fun getLength(): Double = Math.hypot(end.x - start.x, end.y - start.y)
+    Movement()
 }
